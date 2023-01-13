2f27649f346762c18794b94ff747f280e230b747
Kristen Chandler <krismchandler@gmail.com>
2023-01-12 20:51:09 -0600

    created userFlicks and userGenre Data api

---
+++ api/userGenreData.js
@@ -0,0 +1,60 @@
+import { clientCredentials } from '../utils/client';
+
+const dbUrl = clientCredentials.databaseURL;
+
+const getUserGenres = () => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/user_genres`)
+    .then((response) => {
+      if (response.data) {
+        resolve(Object.values(response.data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch((error) => reject(error));
+});
+
+const createUserGenre = (userGenres) => new Promise((resolve, reject) => {
+  const userGenreObj = {
+    user_id: userGenres.userId,
+    genre_id: userGenres.genreId,
+  };
+  fetch(`${dbUrl}/user_genres`, {
+    method: 'POST',
+    body: JSON.stringify(userGenreObj),
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then((response) => resolve(response.json()))
+    .catch((error) => reject(error));
+});
+
+const getSingleUserGenre = (id) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/user_genres/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      resolve({
+        id: data.id,
+        user_id: data.userId,
+        genre_id: data.genreId,
+      });
+    })
+    .catch((error) => reject(error));
+});
+
+const deleteSingleUserGenre = (id) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/user_genres/${id}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((response) => resolve(response))
+    .catch((error) => reject(error));
+});
+
+export {
+  createUserGenre,
+  getSingleUserGenre,
+  deleteSingleUserGenre,
+  getUserGenres,
+};
